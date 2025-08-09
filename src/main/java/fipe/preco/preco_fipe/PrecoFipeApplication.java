bbfd1450850aba1a6e4a4dadd8c4077a28b01255
Davi <davvisamuels@gmail.com>
2025-08-09 17:22:03 -0300

    chore: initial project setup

---
+++ src/main/java/fipe/preco/preco_fipe/PrecoFipeApplication.java
@@ -0,0 +1,13 @@
+package fipe.preco.preco_fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecoFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecoFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fipe/preco/preco_fipe/PrecoFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package fipe.preco.preco_fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecoFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
